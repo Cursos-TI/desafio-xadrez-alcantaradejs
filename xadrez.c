b37c6a7d8e95295c4848c8e3bc907685bfcda2cd
Isaque Alcantara <alcantara.isaque167@gmail.com>
2025-09-10 14:30:23 -0300

    concluido nivel novato (no windows)

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,45 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    printf("+-----------------------------------+\n");
+    printf("|                                   |\n");
+    printf("|         Desafio Xadrez            |\n");
+    printf("|                                   |\n");
+    printf("+-----------------------------------+\n");
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int movimentoTorre = 5; // 5 cassa para direita
+    int movimentoBispo = 5; // 5 cassa na diagonal cima direita
+    int movimentoRainha = 8; // 8 casas para a esquerda
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("\nMovimento do bispo:\n\n");
+    int ib = 1;
+    while (ib <= movimentoBispo) {
+        printf("Cima Direita\n");
+        
+        ib++;
+    }
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("\nMovimento do Torre:\n\n");
+    int it = 1;
+    do {
+        printf("Direita\n");
+        
+        it++;
+    } while (it <= movimentoTorre);
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    printf("\nMovimento da Rainha:\n\n");
+    for (int i = 1; i <= movimentoRainha; i++) {
+        printf("Esquerda\n");
+    }    
+        
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
@@ -27,6 +54,8 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    printf("\n-------------------------------------\n");
 
     return 0;
 }
